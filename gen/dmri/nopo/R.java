5491a3b346862dea5cd356e9a9bcea26c18c6ad7
Anders Boegebeck <ab@it-foqus.dk>
2012-05-02 13:35:57 +0200

    gaaet vaek fra testing af faner

--- gen/dmri/nopo/R.java
+++ gen/dmri/nopo/R.java
@@ -15,7 +15,15 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int alarmtab_notselect=0x7f020000;
+        public static final int alarmtab_select=0x7f020001;
+        public static final int filtertab_notselect=0x7f020002;
+        public static final int filtertab_select=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int logtab_notselect=0x7f020005;
+        public static final int logtab_select=0x7f020006;
+        public static final int menutab_notselect=0x7f020007;
+        public static final int menutab_select=0x7f020008;
     }
     public static final class id {
         public static final int SMSdisplay1=0x7f060001;
